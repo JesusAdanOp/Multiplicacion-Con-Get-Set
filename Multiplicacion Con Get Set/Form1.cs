7e228beea3b991a88220db206f41ebc926f505f0
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-23 08:48:25 -0600

    Agregar archivos de proyecto.

---
+++ Multiplicacion Con Get Set/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplicacion_Con_Get_Set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clmultiplicacion mult = new clmultiplicacion();
+            mult.V1 = int.Parse(txtv1.Text);
+            mult.V2 = int.Parse(txtv2.Text);
+
+            txtres.Text = mult.multiplicar().ToString();
+
+        }
+    }
+}
---
+++ Multiplicacion Con Get Set/clmultiplicacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicacion_Con_Get_Set
+{
+    internal class clmultiplicacion
+    {
+        int v1, v2;
+
+        public int V1
+        {
+            get { return v1; }
+            set { v1 = value; }
+        }
+        public int V2
+        {
+            get { return v2; }
+            set { v2 = value; }
+        }
+
+        public int multiplicar()
+        {
+            return v1*v2;
+        }
+    }
+}
